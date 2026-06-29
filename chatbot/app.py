415ca0bcbc4ab4bf994fd3a9c9dc806eb9dc1d9b
jitesh kumar sharma <sjitesh119@gmail.com>
2026-06-29 11:50:12 +0530

    Add files via upload
    basic chatbot using , gemini api key and ollama model running locally

---
+++ chatbot/app.py
@@ -0,0 +1,40 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# env vaiable call
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["GEMINI_API_KEY"]=os.getenv("GEMINI_API_KEY")
+os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
+
+#langchain_tracing
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+
+#creating chatbot
+
+promt=ChatPromptTemplate.from_messages(
+    [
+        ("system", " you are a helpul assistent. please provide solutions to the users querry"),
+        ("user","question :{question}")
+    ]
+)
+
+# streamlit
+st.title("Langchain  demo with gemini api ")
+input_text=st.text_input("search the topic you want")
+
+# gemini ai llm call
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+output_parser=StrOutputParser()
+
+#chain
+chain=promt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
